83d90f1fae4e72edf473388c257ebb4f2c655b2e
OBYchan <102755726+OBYchan@users.noreply.github.com>
2022-06-13 22:01:13 +0700

    Update leaderboard.js

--- plugins/leaderboard.js
+++ plugins/leaderboard.js
@@ -18,10 +18,10 @@ Kamu: *${usersExp.indexOf(m.sender) + 1}* dari *${usersExp.length}*
 
 ${sortedExp.slice(0, len).map(({ jid, exp }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${exp} Exp*`).join`\n`}
 
-• *Limit Leaderboard Top ${len}* •
-Kamu: *${usersLim.indexOf(m.sender) + 1}* dari *${usersLim.length}*
 
-${sortedLim.slice(0, len).map(({ jid, limit }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${limit} Limit*`).join`\n`}
+
+
+
 
 • *Level Leaderboard Top ${len}* •
 Kamu: *${usersLevel.indexOf(m.sender) + 1}* dari *${usersLevel.length}*
