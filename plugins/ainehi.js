ff3e840b092d667d9abd36b28c0f11c815b50014
OBYchan <102755726+OBYchan@users.noreply.github.com>
2022-06-13 18:57:40 +0700

    Update ainehi.js

--- plugins/ainehi.js
+++ plugins/ainehi.js
@@ -2,12 +2,12 @@ let fs = require('fs')
 const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn }) => {
 let helloaine = fs.readFileSync('./mp3/WhatsApp-Ptt-2021-07-14-at-18.12.33.opus') 
-conn.sendFile(m.chat, helloaine, '', '', m, true)
+conn.sendFile(m.chat, hellosumei, '', '', m, true)
 //conn.sendMessage(m.chat, helloaine, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true})
 // await conn.sendMessage(m.chat, { audio: { url: helloaine }, mimetype: 'audio/mp4'}, m)
 }
 
-handler.customPrefix = /^(hi|hii|hiii|hi aine|hii aine|hiii aine|hy|halo|hallo|helo|hello|hy aine|halo aine|hallo aine|helo aine|hello aine)$/i
+handler.customPrefix = /^(hi|hii|hiii|hi sumei|hii sumei|hiii sumei|hy|halo|hallo|helo|hello|hy sumei|halo sumei|hallo sumei|helo sumei|hello sumei)$/i
 handler.command = new RegExp
 
 handler.limit = true
