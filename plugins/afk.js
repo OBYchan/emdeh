4dbb717dbd85f1793cf2e7912fe9b5d4f9bfcbd8
OBYchan <102755726+OBYchan@users.noreply.github.com>
2022-06-23 14:20:28 +0700

    Update afk.js

--- plugins/afk.js
+++ plugins/afk.js
@@ -9,6 +9,6 @@ ${conn.getName(m.sender)} is now AFK${text ? ': ' + text : ''}
 handler.help = ['afk [alasan]']
 handler.tags = ['main']
 handler.command = /^afk$/i
-handler.limit = true
+handler.premium = true
 
 module.exports = handler
